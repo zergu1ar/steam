bd5291c885e0e24b59c4802f0d61b00e446689ff
Aleksei Loskutov <git@zergular.com>
2020-11-06 11:28:13 +0300

    Fix struct

--- trade.go
+++ trade.go
@@ -85,11 +85,11 @@ type EconAction struct {
 type EconItemDesc struct {
 	ClassID         uint64        `json:"classid,string"`
 	InstanceID      uint64        `json:"instanceid,string"`
-	Tradable        int           `json:"tradable"`
-	BackgroundColor string        `json:"background_color"`
+	Tradable        bool          `json:"tradable"`
+	BackgroundColor *string       `json:"background_color"`
 	IconURL         string        `json:"icon_url"`
 	IconLargeURL    string        `json:"icon_url_large"`
-	IconDragURL     string        `json:"icon_drag_url"`
+	IconDragURL     *string       `json:"icon_drag_url"`
 	Name            string        `json:"name"`
 	NameColor       string        `json:"name_color"`
 	MarketName      string        `json:"market_name"`
